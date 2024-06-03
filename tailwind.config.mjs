9d88aa53c684aa616346144169db37e5d0722010
Mulky Ayyubi <mulky.alayyubi@gmail.com>
2024-06-03 21:58:42 +0700

    hancur

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'sans': ["Barlow", "Arial", "sans-serif"]
+			},
+			colors: {
+				light: "#EDF8F9",
+				dark: "#2E5070",
+				green: "#95BB6F",
+				fuschia: "#C6EAE3"
+			},
+			borderWidth: {
+				10: "10px"
+			}
+		},
 	},
 	plugins: [],
 }
